529fef7aa0d359b2a1fa6fe7a5cf0530ba80a0f0
Aishwarya <99659484+ASHinarretable@users.noreply.github.com>
2026-08-27 14:04:04 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t) : return ""
+        countT = {} #frequency of t
+        window = {} #frequency of current window
+        for i in t:
+            countT[i] = 1 + countT.get(i, 0)
+        have, need, left = 0, len(countT), 0
+        res = [-1, -1]
+        resLen = float("inf")
+        for right in range(len(s)):
+            c = s[right]
+            window[c] = window.get(c,0) + 1
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == need:
+                if (right - left + 1)  < resLen:
+                    res= [left, right]
+                    resLen = (right - left + 1)
+                window[s[left]] -= 1
+                if s[left] in countT and window[s[left]] < countT[s[left]]:
+                    have -= 1
+                left += 1
+        left, right = res
+        return s[left: right + 1] if resLen != float("inf") else ""                   
